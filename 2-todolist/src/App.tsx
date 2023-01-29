28f6c00d8746fcf037825f6dd50e16cce6c35167
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-01-29 23:44:29 +0300

    upd

--- 2-todolist/src/App.tsx
+++ 2-todolist/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
+import TodoList from "./TodoList";
 
 function App() {
+  let tasks = [
+      {id: 1, title: 'HTML', isDone: true},
+      {id: 2, title: 'js', isDone: true},
+      {id: 3, title: '231', isDone: true}
+    ]
+
+const RemoveTask = () =>
+
+
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+           <TodoList title={'What to learn?'}
+           tasks={tasks}
+           />
+
         </div>
     );
 }
